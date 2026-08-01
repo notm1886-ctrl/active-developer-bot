46a99fef18840349b5d00eb7932a500266b8d12c
notm1886-ctrl <notm1886@gmail.com>
2026-08-01 18:29:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Bot started!");
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log(`تم تسجيل الدخول باسم ${client.user.tag}`);
+});
